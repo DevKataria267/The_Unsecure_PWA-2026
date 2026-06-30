1caf5f69a7051099fbe00fbb51a8c7c8081bdde0
DevKataria267 <dev.kataria267@education.nsw.gov.au>
2026-06-30 22:51:14 +1000

    Fixed Broken Authentication

--- user_management.py
+++ user_management.py
@@ -1,28 +1,32 @@
 import sqlite3 as sql
 import time
 import random
+import bcrypt
 
 
 def insertUser(username, password, DoB):
     con = sql.connect("database_files/database.db")
     cur = con.cursor()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     cur.execute(
         "INSERT INTO users (username,password,dateOfBirth) VALUES (?,?,?)",
-        (username, password, DoB),
+        (username, hashed_password, DoB),
     )
     con.commit()
     con.close()
-
 
 def retrieveUsers(username, password):
     con = sql.connect("database_files/database.db")
     cur = con.cursor()
     cur.execute("SELECT * FROM users WHERE username = ?", (username,))
-    if cur.fetchone() == None:
+    user = cur.fetchone()
+    if user == None:
         con.close()
         return False
     else:
-        cur.execute(f"SELECT * FROM users WHERE password = '{password}'")
+        if not bcrypt.checkpw(password.encode('utf-8'), user[2]):
+            con.close()
+            return False
         # Plain text log of visitor count as requested by Unsecure PWA management
         with open("visitor_log.txt", "r") as file:
             number = int(file.read().strip())
@@ -31,12 +35,8 @@ def retrieveUsers(username, password):
             file.write(str(number))
         # Simulate response time of heavy app for testing purposes
         time.sleep(random.randint(80, 90) / 1000)
-        if cur.fetchone() == None:
-            con.close()
-            return False
-        else:
-            con.close()
-            return True
+        con.close()
+        return True
 
 
 def insertFeedback(feedback):
